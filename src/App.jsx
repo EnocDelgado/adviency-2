4cc13f87031cf0516f86cb41c6a98f2bb37b7319
Enoc Delgado <enoc.delgado.technology@gmail.com>
2022-11-29 22:54:25 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='card'>
+                <h1>Gift Card</h1>
+
+                <h5>Candies</h5>
+                <h5>Fruit</h5>
+                <h5>Wine</h5>
+            </div>
+        </div>
+    </>
+  )
+}
